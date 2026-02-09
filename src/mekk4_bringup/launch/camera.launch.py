6573d5d632ed8952bf1ef6af2e30f7b715e66fa8
Emiliam Beke <emiliamb@stud.ntnu.no>
2026-02-09 17:00:05 +0100

    modified:   repos/libcamera_ros.repos 	modified:   src/mekk4_bringup/launch/camera.launch.py 	modified:   src/mekk4_bringup/package.xml

--- src/mekk4_bringup/launch/camera.launch.py
+++ src/mekk4_bringup/launch/camera.launch.py
@@ -7,24 +7,24 @@ from launch_ros.actions import Node
 def generate_launch_description():
     width = LaunchConfiguration("width")
     height = LaunchConfiguration("height")
-    fps = LaunchConfiguration("fps")
+    frame_duration_us = LaunchConfiguration("frame_duration_us")
 
     return LaunchDescription([
         DeclareLaunchArgument("width", default_value="2304"),
         DeclareLaunchArgument("height", default_value="1296"),
-        DeclareLaunchArgument("fps", default_value="15.0"),
+        DeclareLaunchArgument("frame_duration_us", default_value="66666"),
         Node(
-            package="libcamera_ros",
+            package="camera_ros",
             executable="camera_node",
             name="camera_node",
             namespace="camera",
             output="screen",
             parameters=[
-                {"camera_name": "pi_cam"},
+                {"camera": 0},
                 {"frame_id": "camera"},
                 {"width": width},
                 {"height": height},
-                {"frame_rate": fps},
+                {"FrameDurationLimits": [frame_duration_us, frame_duration_us]},
             ],
         ),
     ])
